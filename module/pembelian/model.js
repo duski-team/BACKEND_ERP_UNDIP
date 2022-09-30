f04bd59dbc064406776109c68c251341ce5caef6
faishalyafi <faishalyafiee@gmail.com>
2022-10-01 06:44:06 +0700

    pembelian

---
+++ module/pembelian/model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const {sq} = require('../../config/connection');
+const produk = require('../produk/model');
+const jenisAssetPembelian = require('../m_jenis_aset/model');
+
+const pembelian = sq.define('pembelian', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+    },
+    jumlah_pembelian: {
+        type: DataTypes.INTEGER
+    },
+    tanggal_pembelian: {
+        type: DataTypes.DATE
+    },
+    status_pembelian: {
+        type: DataTypes.INTEGER
+    }
+},
+    {
+        paranoid: true,
+        freezeTableName: true
+    });
+    
+    pembelian.belongsTo(produk,{foreignKey:'produk_id'})
+    produk.hasMany(pembelian,{foreignKey:'produk_id'})
+
+    pembelian.belongsTo(jenisAssetPembelian,{foreignKey:'jenis_asset_pembelian_id'})
+    jenisAssetPembelian.hasMany(pembelian,{foreignKey:'jenis_asset_pembelian_id'})
+
+module.exports = pembelian

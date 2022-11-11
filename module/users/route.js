760b612c58cacc31b8d57074688d33f5996a16d1
faishalyafi <faishalyafiee@gmail.com>
2022-11-11 11:29:02 +0700

    login,register

--- module/users/route.js
+++ module/users/route.js
@@ -8,5 +8,7 @@ router.post('/delete',authentification,Controller.delete);
 router.get('/list',authentification,Controller.list);
 router.get('/detailsById/:id',authentification,Controller.detailsById);
 router.post('/login',Controller.login);
+router.post('/cekEmailUsername',Controller.cekEmailUsername);
+router.post('/listUserAdminCompany',authentification,Controller.listUserAdminCompany);
 
 module.exports = router

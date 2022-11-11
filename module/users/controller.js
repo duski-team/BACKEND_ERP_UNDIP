019f4981ec44e5cb16701247415990d2c560406b
budi-irawan <budi.irawan171717@gmail.com>
2022-11-11 13:26:10 +0700

    acc

--- module/users/controller.js
+++ module/users/controller.js
@@ -1,19 +1,22 @@
 require('dotenv').config({})
-const {sq} = require("../../config/connection");
+const { sq } = require("../../config/connection");
 const { v4: uuid_v4 } = require("uuid");
 const users = require("./model");
 const company = require("../company_usaha/model")
+const coa5 = require("../coa5/model")
+const coa6 = require("../coa6/model")
 const { QueryTypes, Op } = require('sequelize');
-const s = {type:QueryTypes.SELECT};
+const s = { type: QueryTypes.SELECT };
 const bcrypt = require('../../helper/bcrypt');
 const jwt = require('../../helper/jwt');
+const { token } = require('morgan');
 
-async function createSuperUser(){
+async function createSuperUser() {
     let encryptedPassword = bcrypt.hashPassword(process.env.ADMIN);
     await company.findOrCreate({
-        where:{id:"UNDIP"},
-        defaults:{
-            id:"UNDIP",
+        where: { id: "UNDIP" },
+        defaults: {
+            id: "UNDIP",
             nama_usaha: "MASTER",
             nama_pengelola: "UNDIP",
             code: "UNDIP"
@@ -21,11 +24,11 @@ async function createSuperUser(){
 
     })
     await users.findOrCreate({
-        where:{username:"erp_admin"},
-        defaults:{
-            id:"superadmin",
-            username:"erp_admin",
-            email:"admin@gmail.com",
+        where: { username: "erp_admin" },
+        defaults: {
+            id: "superadmin",
+            username: "erp_admin",
+            email: "admin@gmail.com",
             password: encryptedPassword,
             company_id: "UNDIP"
         }
@@ -36,16 +39,16 @@ createSuperUser()
 
 class Controller {
 
-    static  async register (req,res){
-        const {email,username,firstname,lastname,phone_no,password,register_token,resetpassword_token,variant,priority,jenis_user_id,nama_usaha,location,code}= req.body
+    static async register(req, res) {
+        const { email, username, firstname, lastname, phone_no, password, register_token, resetpassword_token, variant, priority, jenis_user_id, nama_usaha, location, code } = req.body
 
         try {
-            let cekCompany = await company.findAll({where:{[Op.or]:[{nama_usaha},{code}]}});
-            let cekUser = await users.findAll({where:{[Op.or]:[{email},{username}]}});
+            let cekCompany = await company.findAll({ where: { [Op.or]: [{ nama_usaha }, { code }] } });
+            let cekUser = await users.findAll({ where: { [Op.or]: [{ email }, { username }] } });
 
-            if(cekCompany.length>0 || cekUser.length>0){
+            if (cekCompany.length > 0 || cekUser.length > 0) {
                 res.status(201).json({ status: 204, message: "data sudah ada" });
-            }else{
+            } else {
                 let profil_image = "";
 
                 if (req.files) {
@@ -54,10 +57,10 @@ class Controller {
                     }
                 }
                 let encryptedPassword = bcrypt.hashPassword(password);
-                let perusahan_id = await company.create({id:uuid_v4(),nama_usaha,location,code})
-                let data = await users.create({id:uuid_v4(),email,username,firstname,lastname,phone_no,password:encryptedPassword,register_token,resetpassword_token,variant,priority,profil_image,jenis_user_id,company_id:perusahan_id[0].id})
+                let perusahan_id = await company.create({ id: uuid_v4(), nama_usaha, location, code })
+                let data = await users.create({ id: uuid_v4(), email, username, firstname, lastname, phone_no, password: encryptedPassword, register_token, resetpassword_token, variant, priority, profil_image, jenis_user_id, company_id: perusahan_id[0].id })
 
-                res.status(200).json({ status: 200, message: "sukses",data });
+                res.status(200).json({ status: 200, message: "sukses", data });
             }
         } catch (err) {
             console.log(req.body);
@@ -66,107 +69,147 @@ class Controller {
         }
     }
 
-    static update (req,res){
-        const {id,email,username,firstname,lastname,phone_no,password,register_token,resetpassword_token,variant,priority,jenis_user_id,company_id}= req.body
-        
+    static update(req, res) {
+        const { id, email, username, firstname, lastname, phone_no, password, register_token, resetpassword_token, variant, priority, jenis_user_id, company_id } = req.body
+
         if (req.files) {
             if (req.files.file1) {
                 let profil_image = req.files.file1[0].filename;
-                users.update({profil_image},{where:{id}})
+                users.update({ profil_image }, { where: { id } })
             }
         }
 
-        users.update({email,username,firstname,lastname,phone_no,password,register_token,resetpassword_token,variant,priority,jenis_user_id,company_id},{where:{id}}).then(data =>{
+        users.update({ email, username, firstname, lastname, phone_no, password, register_token, resetpassword_token, variant, priority, jenis_user_id, company_id }, { where: { id } }).then(data => {
             res.status(200).json({ status: 200, message: "sukses" });
-        }).catch(err =>{
+        }).catch(err => {
             console.log(req.body);
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         })
     }
 
-    static delete (req,res){
-        const {id}= req.body
+    static delete(req, res) {
+        const { id } = req.body
 
-        users.destroy({where:{id}}).then(data =>{
+        users.destroy({ where: { id } }).then(data => {
             res.status(200).json({ status: 200, message: "sukses" });
-        }).catch(err =>{
+        }).catch(err => {
             console.log(req.body);
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         })
     }
 
-    static async list (req,res){
+    static async list(req, res) {
         try {
-            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL order by u."createdAt" desc`,s);
+            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL order by u."createdAt" desc`, s);
 
-            res.status(200).json({ status: 200, message: "sukses",data });
+            res.status(200).json({ status: 200, message: "sukses", data });
         } catch (err) {
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         }
     }
 
-    static async listUserByCompanyId (req,res){
-        const {company_id} = req.body
+    static async listUserByCompanyId(req, res) {
+        const { company_id } = req.body
         try {
-            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" isnull and u.company_id = '${company_id}' order by u."createdAt" desc`,s);
+            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" isnull and u.company_id = '${company_id}' order by u."createdAt" desc`, s);
 
-            res.status(200).json({ status: 200, message: "sukses",data });
+            res.status(200).json({ status: 200, message: "sukses", data });
         } catch (err) {
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         }
     }
 
-    static async listUserByJenisUserId (req,res){
-        const {jenis_user_id} = req.body
+    static async listUserByJenisUserId(req, res) {
+        const { jenis_user_id } = req.body
         try {
-            let data = await sq.query(`SELECT u.id as user_id * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL and and u.jenis_user_id = '${jenis_user_id}' order by u."createdAt" desc`,s);
+            let data = await sq.query(`SELECT u.id as user_id * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL and and u.jenis_user_id = '${jenis_user_id}' order by u."createdAt" desc`, s);
 
-            res.status(200).json({ status: 200, message: "sukses",data });
+            res.status(200).json({ status: 200, message: "sukses", data });
         } catch (err) {
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         }
     }
 
-    static async detailsById (req,res){
-        const {id} = req.params
+    static async detailsById(req, res) {
+        const { id } = req.params
 
         try {
-            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL and u.id = '${id}'`,s);
+            let data = await sq.query(`SELECT u.id as user_id, * FROM users u join jenis_user ju on ju.id = u.jenis_user_id join company_usaha cu on cu.id = u.company_id where u."deletedAt" ISNULL and u.id = '${id}'`, s);
 
-            res.status(200).json({ status: 200, message: "sukses",data });
+            res.status(200).json({ status: 200, message: "sukses", data });
         } catch (err) {
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         }
     }
 
-    static login (req,res){
-        const {username,password} = req.body
+    static login(req, res) {
+        const { username, password } = req.body
 
-        users.findAll({where:{username}}).then(data =>{
-            if(data.length == 0){
+        users.findAll({ where: { username } }).then(data => {
+            if (data.length == 0) {
                 res.status(200).json({ status: 200, message: "username Tidak Terdaftar" });
-            }else{
+            } else {
                 let dataToken = {
                     id: data[0].id,
                     password: data[0].password
                 };
                 let hasil = bcrypt.compare(password, data[0].dataValues.password);
-                if(hasil){
-                    res.status(200).json({status: 200,message: "sukses",token: jwt.generateToken(dataToken),id: data[0].id});
-                }else{
+                if (hasil) {
+                    res.status(200).json({ status: 200, message: "sukses", token: jwt.generateToken(dataToken), id: data[0].id });
+                } else {
                     res.status(200).json({ status: 200, message: "Password Salah" });
                 }
             }
-        }).catch(err =>{
+        }).catch(err => {
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
         })
+    }
+
+    static async aceptedUser(req, res) {
+        const { id } = req.body
+
+        try {
+            const t = await sq.transaction()
+
+            let cekUser = await sq.query(`select * from users u where u."deletedAt" isnull and u.id = '${id}'`, s)
+            // console.log(cekUser[0].status_users);
+            if (cekUser[0].status_users == 1) {
+                let cekCoa5 = await sq.query(`select * from coa5 c where c."deletedAt" isnull and c.company_id = 'UNDIP' order by c.kode_coa5`, s)
+                let cekCoa6 = await sq.query(`select * from coa6 c where c.coa5_id in (select c2.id from coa5 c2 where c2.company_id = 'UNDIP' order by c2.kode_coa5) `, s)
+                for (let i = 0; i < cekCoa5.length; i++) {
+                    let idCoa5 = uuid_v4()
+                    for (let j = 0; j < cekCoa6.length; j++) {
+                        if (cekCoa6[j].coa5_id == cekCoa5[i].id) {
+                            cekCoa6[j].id = uuid_v4()
+                            cekCoa6[j].coa5_id = idCoa5
+                        }
+                    }
+                    cekCoa5[i].id = idCoa5
+                    cekCoa5[i].company_id = cekUser[0].company_id
+
+                }
+                // console.log(cekCoa5);
+                // console.log(cekCoa6);
+                await users.update({ status_user: 2 }, { where: { id }, transaction: t })
+                await coa5.bulkCreate(cekCoa5, { transaction: t })
+                await coa6.bulkCreate(cekCoa6, { transaction: t })
+                await t.commit()
+                res.status(200).json({ status: 200, message: "sukses" });
+            } else {
+                res.status(200).json({ status: 200, message: "status user bukan 1" });
+            }
+        } catch (err) {
+            await t.rollback()
+            console.log(err);
+            res.status(500).json({ status: 500, message: "gagal", data: err });
+        }
     }
 }
 module.exports = Controller;
--- module/users/route.js
+++ module/users/route.js
@@ -1,12 +1,13 @@
-const Controller = require( './controller' );
-const router = require( 'express' ).Router();
+const Controller = require('./controller');
+const router = require('express').Router();
 const authentification = require('../../middleware/authentification')
 
-router.post('/register',Controller.register);
-router.post('/update',authentification,Controller.update);
-router.post('/delete',authentification,Controller.delete);
-router.get('/list',authentification,Controller.list);
-router.get('/detailsById/:id',authentification,Controller.detailsById);
-router.post('/login',Controller.login);
+router.post('/register', Controller.register);
+router.post('/update', authentification, Controller.update);
+router.post('/delete', authentification, Controller.delete);
+router.get('/list', authentification, Controller.list);
+router.get('/detailsById/:id', authentification, Controller.detailsById);
+router.post('/login', Controller.login);
+router.post('/aceptedUser', authentification, Controller.aceptedUser);
 
 module.exports = router

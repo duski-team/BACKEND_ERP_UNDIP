c17425ef4c5b9eae634fd7619cbf9f4aa9eea5dd
satriopurbo <satrio_leonheart@yahoo.com>
2022-11-11 08:59:54 +0700

    asd

--- module/test/route.js
+++ module/test/route.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const authentification = require('../../middleware/authentification');
 
 router.post('/test', Controller.test);
+router.post('/coa6', Controller.coa6);
 
 
 module.exports = router

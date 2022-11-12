17af249c5291651b420f7b3cf30d7370fb4428bc
budi-irawan <budi.irawan171717@gmail.com>
2022-11-12 10:34:50 +0700

    cel

--- module/company_usaha/route.js
+++ module/company_usaha/route.js
@@ -8,6 +8,6 @@ router.post('/update', authentification, upload, Controller.update);
 router.post('/delete', authentification, Controller.delete);
 router.get('/list', authentification, Controller.list);
 router.get('/detailsById/:id', authentification, Controller.detailsById);
-router.post('/cekCompany', authentification, Controller.cekCompany);
+router.post('/cekCompany', Controller.cekCompany);
 
 module.exports = router

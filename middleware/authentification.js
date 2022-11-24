bfb5502496877261225b94eebdd3c83d51b79427
faishalyafi <faishalyafiee@gmail.om>
2022-11-24 19:47:04 +0700

    fix authen

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -3,7 +3,9 @@ const user = require('../module/users/model')
 
 async function authentification (req,res,next){
     try {
-        let decode = verifyToken(req.headers.token);
+        let authToken = req.headers.authorization
+        let token = authToken.split(' ')[1]
+        let decode = verifyToken(token);
         let masterUser = await user.findAll({where:{id:decode.id,password:decode.password}})
         if(masterUser.length==0){
             res.status(201).json({ status: 201, message: "anda belum login" });

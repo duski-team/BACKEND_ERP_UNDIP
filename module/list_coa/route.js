02647fd7f460691ef9c6b610881cd20ee03b7f89
faishalyafi <faishalyafiee@gmail.om>
2022-11-25 19:46:07 +0700

    list_coa

---
+++ module/list_coa/route.js
@@ -0,0 +1,13 @@
+const Controller = require('./controller');
+const router = require('express').Router();
+const authentification = require('../../middleware/authentification');
+
+//! COA1
+//! COA2
+router.post('/register', authentification, Controller.register);
+//! COA3
+//! COA4
+//! COA5
+//! COA6
+
+module.exports = router

7f624a3dfa50dc84d7ae5c6abe7b85669a21d288
faishalyafi <faishalyafiee@gmail.om>
2022-11-27 23:30:13 +0700

    fix

--- module/pembelian/controller.js
+++ module/pembelian/controller.js
@@ -1,33 +1,58 @@
 const { sq } = require("../../config/connection");
 const { v4: uuid_v4 } = require("uuid");
 const pembelian = require("./model");
+const trxPembelian = require("../trx_pembelian/model");
 const { QueryTypes } = require('sequelize');
 const s = { type: QueryTypes.SELECT };
 
 
 class Controller {
 
-    static register(req, res) {
-        const { jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id } = req.body
+    static async register(req, res) {
+        const { jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id,satuan_txp,harga_satuan_txp,harga_total_txp } = req.body;
 
-        pembelian.create({ id: uuid_v4(), jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id }).then(data => {
-            res.status(200).json({ status: 200, message: "sukses", data });
-        }).catch(err => {
+        const t = await sq.transaction();
+        try {
+            let pembelian_id = uuid_v4()
+            let hasil = await pembelian.create({ id:pembelian_id , jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id },{transaction:t});
+            await trxPembelian.create({ id: uuid_v4(),jumlah_txp:jumlah_pembelian,satuan_txp,harga_satuan_txp,harga_total_txp,pembelian_id },{transaction:t});
+            await t.commit();
+
+            res.status(200).json({ status: 200, message: "sukses", data:hasil });
+        } catch (err) {
+            await t.rollback();
             console.log(req.body);
             console.log(err);
             res.status(500).json({ status: 500, message: "gagal", data: err });
-        })
+        }
     }
 
-    static update(req, res) {
-        const { id, jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id } = req.body
+    static async update(req, res) {
+        const { id, jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id,satuan_txp,harga_satuan_txp,harga_total_txp } = req.body
 
+        const t = await sq.transaction();
+
+        try {
+            let cekPembelian = await trxPembelian.findAll({where:{pembelian_id:id}})
+            if(cekPembelian[0].status_persetujuan_txp == 2){
+                res.status(201).json({ status: 204, message: "status trx pembelian bukan 1" });
+            }else{
+                await pembelian.update({ jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id }, { where: { id },transaction:t });
+                await trxPembelian.create({jumlah_txp:jumlah_pembelian,satuan_txp,harga_satuan_txp,harga_total_txp },{where:{pembelian_id:id},transaction:t});
+                await t.commit();
+
+                res.status(200).json({ status: 200, message: "sukses" });
+            }
+        } catch (err) {
+            await t.rollback();
+            console.log(req.body);
+            console.log(err);
+            res.status(500).json({ status: 500, message: "gagal", data: err });
+        }
         pembelian.update({ jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id }, { where: { id } }).then(data => {
             res.status(200).json({ status: 200, message: "sukses" });
         }).catch(err => {
-            console.log(req.body);
-            console.log(err);
-            res.status(500).json({ status: 500, message: "gagal", data: err });
+            
         })
     }
 

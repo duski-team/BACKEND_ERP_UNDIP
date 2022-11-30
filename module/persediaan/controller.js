aaf7cbb589b5e913c1b514beaeef926b0e70a0c1
faishalyafi <faishalyafiee@gmail.com>
2022-11-30 16:39:06 +0700

    fix

--- module/persediaan/controller.js
+++ module/persediaan/controller.js
@@ -43,7 +43,7 @@ class Controller {
     }
 
     static async registerSaldoAwal(req, res) {
-        let { nama_persediaan, kode_persediaan, satuan_persedian, harga_jual, stock, stock_rusak, harga_satuan, tanggal_saldo_awal, kondisi, keterangan, coa6_id, kategori_id, sub_kategori_id, sub_sub_kategori_id,company_id } = req.body
+        let { nama_persediaan, kode_persediaan, satuan_persedian, harga_jual, stock, stock_rusak, harga_satuan, tanggal_saldo_awal, kondisi, keterangan, coa6_id, kategori_id, sub_kategori_id, sub_sub_kategori_id,company_id, nominal_coa6} = req.body
 
         const t = await sq.transaction();
 
@@ -65,7 +65,7 @@ class Controller {
             }else{
                 let akunModal = await sq.query(`select c6.*,gl.akun_id,gl.sisa_saldo from coa6 c6 join coa5 c5 on c5.id = c6.coa5_id left join general_ledger gl on gl.akun_id = c6.id where c6."deletedAt" isnull and c5.company_id = '${company_id}' and c6.kode_coa6 = '3.1.1.1.01.0001' order by gl."createdAt" desc limit 1`,s);
 
-                let sisa_saldo = stock * harga_jual
+                let sisa_saldo = nominal_coa6
                 let saldoModal = sisa_saldo
                 if(akunModal[0].sisa_saldo){
                     saldoModal= akunModal[0].sisa_saldo + sisa_saldo
@@ -88,6 +88,53 @@ class Controller {
             res.status(500).json({ status: 500, message: "gagal", data: err });
         }
     }
+
+    // static async registerSaldoAwal(req, res) {
+    //     let { nama_persediaan, kode_persediaan, satuan_persedian, harga_jual, stock, stock_rusak, harga_satuan, tanggal_saldo_awal, kondisi, keterangan, coa6_id, kategori_id, sub_kategori_id, sub_sub_kategori_id,company_id } = req.body
+
+    //     const t = await sq.transaction();
+
+    //     try {
+    //         if(!company_id){
+    //             company_id = req.dataUsers.company_id
+    //         }
+    //         let gambar = "";
+
+    //         if (req.files) {
+    //             if (req.files.file1) {
+    //                     gambar = req.files.file1[0].filename;
+    //             }
+    //         }
+    //         const cekPersediaan = await persediaan.findAll({ where: { kode_persediaan,company_id,coa6_id } });
+
+    //         if(cekPersediaan.length>0){
+    //             res.status(201).json({ status: 204, message: "data sudah ada" });
+    //         }else{
+    //             let akunModal = await sq.query(`select c6.*,gl.akun_id,gl.sisa_saldo from coa6 c6 join coa5 c5 on c5.id = c6.coa5_id left join general_ledger gl on gl.akun_id = c6.id where c6."deletedAt" isnull and c5.company_id = '${company_id}' and c6.kode_coa6 = '3.1.1.1.01.0001' order by gl."createdAt" desc limit 1`,s);
+
+    //             let sisa_saldo = stock * harga_jual
+    //             let saldoModal = sisa_saldo
+    //             if(akunModal[0].sisa_saldo){
+    //                 saldoModal= akunModal[0].sisa_saldo + sisa_saldo
+    //             }
+    //             let barang = {id:uuid_v4(),tanggal_transaksi:tanggal_saldo_awal,sisa_saldo:sisa_saldo,penambahan:sisa_saldo,status:1,akun_id:coa6_id,akun_pasangan_id:akunModal[0].id}
+    //             let modal = {id:uuid_v4(),tanggal_transaksi:tanggal_saldo_awal,sisa_saldo:saldoModal,penambahan:sisa_saldo,status:1,akun_id:akunModal[0].id,akun_pasangan_id:coa6_id}
+
+    //             let data = await persediaan.create({ id:uuid_v4(), nama_persediaan, kode_persediaan, satuan_persedian, harga_jual, stock, stock_rusak, harga_satuan, tanggal_saldo_awal, kondisi, keterangan, coa6_id, kategori_id, sub_kategori_id, sub_sub_kategori_id, gambar, company_id },{transaction:t});
+    //             await trxPembelian.create({id:uuid_v4(),tgl_persetujuan_manajer_txp:tanggal_saldo_awal,jumlah_txp:stock,satuan_txp:satuan_persedian,tgl_persetujuan_akuntan_txp:tanggal_saldo_awal,status_persetujuan_txp:3,harga_satuan_txp:0,harga_total_txp:0},{transaction:t});
+    //             await generalLedger.bulkCreate([barang,modal],{transaction:t});
+               
+    //             await t.commit();
+
+    //             res.status(200).json({ status: 200, message: "sukses",data });
+    //         }
+    //     } catch (err) {
+    //         await t.rollback();
+    //         console.log(req.body);
+    //         console.log(err);
+    //         res.status(500).json({ status: 500, message: "gagal", data: err });
+    //     }
+    // }
 
     static async update(req, res) {
         const { id, nama_persediaan, kode_persediaan, satuan_persedian, harga_jual, stock, stock_rusak, harga_satuan, tanggal_saldo_awal, kondisi, keterangan, coa6_id, kategori_id, sub_kategori_id, sub_sub_kategori_id, company_id } = req.body

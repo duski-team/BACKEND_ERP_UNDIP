3334f920917d4aec4c6aa806f103308c4ec712be
faishalyafi <faishalyafiee@gmail.com>
2022-11-30 17:11:29 +0700

    register asset

--- module/pembelian/controller.js
+++ module/pembelian/controller.js
@@ -61,6 +61,28 @@ class Controller {
         }
     }
 
+    static async registerAset(req, res) {
+        let { jumlah_pembelian, tanggal_pembelian, status_pembelian, jenis_asset_pembelian_id, vendor_id,satuan_txp,harga_satuan_txp,harga_total_txp, company_id,coa6_id } = req.body;
+
+        const t = await sq.transaction();
+        try {
+            if(!company_id){
+                company_id = req.dataUsers.company_id
+            }
+            let pembelian_id = uuid_v4()
+            let hasil = await pembelian.create({ id:pembelian_id , jumlah_pembelian, tanggal_pembelian, status_pembelian, jenis_asset_pembelian_id, vendor_id, company_id,coa6_id },{transaction:t});
+            await trxPembelian.create({ id: uuid_v4(),jumlah_txp:jumlah_pembelian,satuan_txp,harga_satuan_txp,harga_total_txp,pembelian_id },{transaction:t});
+            await t.commit();
+
+            res.status(200).json({ status: 200, message: "sukses", data:hasil });
+        } catch (err) {
+            await t.rollback();
+            console.log(req.body);
+            console.log(err);
+            res.status(500).json({ status: 500, message: "gagal", data: err });
+        }
+    }
+
     static async update(req, res) {
         let { id, jumlah_pembelian, tanggal_pembelian, status_pembelian, persediaan_id, jenis_asset_pembelian_id, vendor_id,satuan_txp,harga_satuan_txp,harga_total_txp, company_id,coa6_id } = req.body
 
@@ -70,7 +92,7 @@ class Controller {
             if(!company_id){
                 company_id = req.dataUsers.company_id
             }
-            
+
             let cekPembelian = await trxPembelian.findAll({where:{pembelian_id:id}})
 
             if(cekPembelian[0].status_persetujuan_txp == 1){
@@ -153,7 +175,7 @@ class Controller {
     static async listPembelianByCompanyId(req, res) {
         const { company_id } = req.body
         try {
-            let data = await sq.query(`select p.id as pembelian_id, * , c6.nama_coa6 as "nama_aset" from pembelian p left join persediaan p2 on p2.id = p.persediaan_id left join m_jenis_aset mja on p.jenis_asset_pembelian_id = mja.id left join master_vendor mv on mv.id = p.vendor_id left join coa6 c6 on c6.id = p.coa6_id where p."deletedAt" isnull and p.company_id = '${company_id}' order by p."createdAt" desc `, s);
+            let data = await sq.query(`select p.id as pembelian_id, * , c6.nama_coa6 as "nama_aset" from pembelian p left join persediaan p2 on p2.id = p.persediaan_id left join m_jenis_aset mja on p.jenis_asset_pembelian_id = mja.id left join master_vendor mv on mv.id = p.vendor_id left join coa6 c6 on c6.id = p.coa6_id left join trx_pembelian tp on tp.pembelian_id = p.id where p."deletedAt" isnull and p.company_id = '${company_id}' order by p."createdAt" desc`, s);
 
             res.status(200).json({ status: 200, message: "sukses", data });
         } catch (err) {
--- module/pembelian/route.js
+++ module/pembelian/route.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const authentification = require('../../middleware/authentification');
 
 router.post('/register', authentification, Controller.register);
+router.post('/registerAset', authentification, Controller.registerAset);
 router.post('/update', authentification, Controller.update);
 router.post('/delete', authentification, Controller.delete);
 router.get('/list', authentification, Controller.list);

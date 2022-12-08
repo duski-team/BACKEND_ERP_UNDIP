34b91598be7ef31c3b2b2d51d2a59f06d5330fbf
faishalyafi <faishalyafiee@gmail.om>
2022-12-08 15:44:50 +0700

    fix

--- module/trx_pengeluaran_kas/route.js
+++ module/trx_pengeluaran_kas/route.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const authentification = require('../../middleware/authentification');
 
 router.post('/register', authentification, Controller.register);
+router.post('/registerKewajibanLainKepadaVendor', authentification, Controller.registerKewajibanLainKepadaVendor);
 router.post('/update', authentification, Controller.update);
 router.post('/delete', authentification, Controller.delete);
 router.get('/list', authentification, Controller.list);
